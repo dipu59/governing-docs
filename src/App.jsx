14a159026d705f698d7098c39590236205d9cdb6
dipu59 <12biswasdipu@gmail.com>
2025-04-24 19:58:56 +0530

    Hero Secion Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import Hero from "./components/home/hero";
 import Footer from "./components/shared/footer";
 import Navber from "./components/shared/navber";
 
 function App() {
   return (
-    <main className="max-w-[1440px] min-w-[375px] px-4  mx-auto h-screen">
+    <main className="max-w-[1440px] min-w-[375px] px-4 bg-[url(/hero-bg.svg)] bg-no-repeat object-cover bg-left-top  mx-auto  font-default">
       <div className="">
         <Navber />
-        <Footer/>
+        <Hero/>
+        {/* <Footer/> */}
       </div>
     </main>
   );
---
+++ src/components/home/hero.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button } from "../Common/button";
+import hero from "/hero-side.svg";
+function Hero() {
+  return (
+    <section className="flex lg:flex-row flex-col-reverse  justify-between items-center gap-3 px-3 lg:pt-[142px] md:pt-[100px] sm:pt-[50px] pt-[40px]">
+      <div className=" lg:space-y-[29px] md:space-y-[20px] mt-10 lg:mt-0 space-y-3 lg:pl-[128px]">
+        <h1 className="font-bold 2xl:text-[60px] lg:text-[55px] md:text-[50px] text-[33px] sm:text-[40px] max-w-[593px] font-heading text-secondery  leading-snug">
+          Simplify Your Real Estate Document Analysis
+        </h1>
+        <p className="lg:text-xl md:text-lg sm:text-base text-sm max-w-[519px] text-secondery font-semibold">
+          Streamline, Automate, and Secure Your Business Documents with Ai
+          GoverningDocs
+        </p>
+        <Button>Get Started Today</Button>
+      </div>
+      <div>
+        <img src={hero} alt="" />
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
