f1b12df18988618d9d00b15ea9042933d6f61d4c
dipu59 <12biswasdipu@gmail.com>
2025-04-25 13:53:47 +0530

    problem fix

---
+++ src/components/home/review.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { GradientTitle } from '../Common/gradient-title'
+
+export default function Reviews() {
+  return (
+      <main className='my-[150px] py-[80px] bg-[#F6F6F6] '>
+          <GradientTitle gradient='Our Client' className=' text-center ' >Reviews From</GradientTitle>
+    </main>
+  )
+}
