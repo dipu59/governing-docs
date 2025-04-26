d1cc847dce538a8c6cdf149303a351d26c9b9f11
dipu59 <12biswasdipu@gmail.com>
2025-04-26 23:37:37 +0530

    problem fix

--- src/components/home/clientinfo.jsx
+++ src/components/home/clientinfo.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export  const ClientInfo = ({url, name}) => {
+export  const ClientInfo = ({url, name, reaction}) => {
   return (
     <div className="gradient_border flex-grow w-full  bg-white rounded-[20px] h-[91px] ">
     <div className="flex gap-[13px] justify-start items-center">
       <img src={url} alt="" />
       <div className="text-grya font-semibold">
         <h4>{name}</h4>
-        <h4>Happy Client</h4>
+        <h4>{reaction}</h4>
       </div>
     </div>
   </div>
